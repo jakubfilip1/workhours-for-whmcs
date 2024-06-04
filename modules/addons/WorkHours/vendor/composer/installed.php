3807d7fb7b994df58f982288468e1fa884ae3183
Jakub Filip <jakub.fi@modulesgarden.com>
2024-06-04 20:41:14 +0200

    composer

---
+++ modules/addons/WorkHours/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jakub-filip/work-hours',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ea4be82767e707a0347bc44bb1b30cbb6e4901c5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jakub-filip/work-hours' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ea4be82767e707a0347bc44bb1b30cbb6e4901c5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
